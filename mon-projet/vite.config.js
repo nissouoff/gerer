38bb759027d9c6654c14aa1d1734a0fa50e9e352
nissouoff <156860993+nissouoff@users.noreply.github.com>
2025-09-30 23:49:33 +0100

    Update vite.config.js

--- mon-projet/vite.config.js
+++ mon-projet/vite.config.js
@@ -1,12 +1,11 @@
-// vite.config.js
 import { defineConfig } from "vite";
 
 export default defineConfig({
   server: {
     host: "0.0.0.0",
-    port: 5173,
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
     allowedHosts: [
-      ".onrender.com", // autorise tous les sous-domaines ngrok
+      ".onrender.com", // autorise tous les sous-domaines Render
     ],
   },
 });
